4cd3823e7220772ae3aa3c40d6b1be44eab9e34c
infFITSDevelopment <inffits.development@gmail.com>
2024-12-26 17:06:02 +0800

    Update ts-ai-pd.js v2

--- ts-ai-pd.js
+++ ts-ai-pd.js
@@ -1,51 +1,51 @@
 ;(function ($) {
-  // 動態添加 Google 字體連結
-  var googleFontLink = document.createElement("link");
-  googleFontLink.rel = "preconnect";
-  googleFontLink.href = "https://fonts.googleapis.com";
-  document.head.appendChild(googleFontLink);
+      // 動態添加 Google 字體連結
+      var googleFontLink = document.createElement("link");
+      googleFontLink.rel = "preconnect";
+      googleFontLink.href = "https://fonts.googleapis.com";
+      document.head.appendChild(googleFontLink);
 
-  var googleFontLink2 = document.createElement("link");
-  googleFontLink2.rel = "preconnect";
-  googleFontLink2.href = "https://fonts.gstatic.com";
-  googleFontLink2.crossorigin = "anonymous";
-  document.head.appendChild(googleFontLink2);
+      var googleFontLink2 = document.createElement("link");
+      googleFontLink2.rel = "preconnect";
+      googleFontLink2.href = "https://fonts.gstatic.com";
+      googleFontLink2.crossorigin = "anonymous";
+      document.head.appendChild(googleFontLink2);
 
-  var googleFontLink3 = document.createElement("link");
-  googleFontLink3.rel = "stylesheet";
-  googleFontLink3.href =
-    "https://fonts.googleapis.com/css2?family=Chocolate+Classical+Sans&family=Figtree:ital,wght@0,300..900;1,300..900&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap";
-  document.head.appendChild(googleFontLink3);
+      var googleFontLink3 = document.createElement("link");
+      googleFontLink3.rel = "stylesheet";
+      googleFontLink3.href =
+        "https://fonts.googleapis.com/css2?family=Chocolate+Classical+Sans&family=Figtree:ital,wght@0,300..900;1,300..900&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap";
+      document.head.appendChild(googleFontLink3);
 
-  // 動態添加自定 CSS
-  var customCSS = document.createElement("link");
-  customCSS.rel = "stylesheet";
-  customCSS.type = "text/css";
-  // customCSS.href = "./../css/iframe_ai_pd_style.css";
-  customCSS.href = "https://cdn.jsdelivr.net/gh/lmybs112/ts-iframe@1.0.8/css/iframe_ai_pd_style.css";
-  document.head.appendChild(customCSS);
+      // 動態添加自定 CSS
+      var customCSS = document.createElement("link");
+      customCSS.rel = "stylesheet";
+      customCSS.type = "text/css";
+      customCSS.href = "./../css/iframe_ai_pd_style.css";
+      //   customCSS.href = "https://cdn.jsdelivr.net/gh/lmybs112/ts-iframe@1.0.8/css/iframe_ai_pd_style.css";
+      document.head.appendChild(customCSS);
 
-  $(function () {
-    console.log("DOM is ready");
-    $(window).on("message", function (event) {
-      var origin = event.originalEvent.origin;
-      // if (origin !== 'https://iframe所在的網址') return; // 避免跨域攻擊
-      // 處理從 <iframe> 來的訊息
-      // console.log("接收到來自 iframe 的訊息：", event.originalEvent.data);
-      if (event.originalEvent.data.type === "result") {
-        if (event.originalEvent.data.value) {
-          $(".ai-pd-container__trigger").addClass(
-            "ai-pd-container__trigger--result"
-          );
-        } else {
-          $(".ai-pd-container__trigger").removeClass(
-            "ai-pd-container__trigger--result"
-          );
-        }
-      }
-    });
-    // 添加 html template
-    var aiSearchPdTemplate = `
+      $(function () {
+        console.log("DOM is ready");
+        $(window).on("message", function (event) {
+          var origin = event.originalEvent.origin;
+          // if (origin !== 'https://iframe所在的網址') return; // 避免跨域攻擊
+          // 處理從 <iframe> 來的訊息
+          // console.log("接收到來自 iframe 的訊息：", event.originalEvent.data);
+          if (event.originalEvent.data.type === "result") {
+            if (event.originalEvent.data.value) {
+              $(".ai-pd-container__trigger").addClass(
+                "ai-pd-container__trigger--result"
+              );
+            } else {
+              $(".ai-pd-container__trigger").removeClass(
+                "ai-pd-container__trigger--result"
+              );
+            }
+          }
+        });
+        // 添加 html template
+        var aiSearchPdTemplate = `
             <div class="ai-pd-container">
             <button
                 class="ai-pd-container__trigger ai-pd-container__trigger--search"
@@ -72,27 +72,24 @@
         style="
           z-index: 60;
           display: block;
-          position: absolute;
-          top: 0;
-          bottom: 0;
-          left: 0;
-          right: 0;
-          margin: auto;
+          position: absolute; inset: 0;
         "
       >
-        <div id="tryon">
+        <div id="tryon" style="height: 100%;width:100%;display:flex;justify-content:center;align-items:center;">
           <iframe
             id="inffits_tryon_window"
             style="
-              width: 100%;
               height: 100%;
-              visibility: visible;
-              position: relative;
-              border: none;
-              outline: none;
-              z-index: 14;
+                width: 100%;
+                visibility: visible;
+                position: relative;
+                border: none;
+                outline: none;
+                z-index: 14;
+                max-width: 95vw;
+                margin: 0 auto;
             "
-           src="https://ts-iframe-8ysy.vercel.app/iframe_container_module.html"
+           src="./iframe_container_module.html"
           ></iframe>
 
         </div>
@@ -120,7 +117,7 @@
         right: 0;
         bottom: 0;
         height: 580px;
-        width: 355px !important;
+        // width: 355px !important;
       }
       #tryon {
         margin: auto;
@@ -129,33 +126,54 @@
       }
     </style>
       `;
-    document.body.insertAdjacentHTML("beforeend", aiSearchPdTemplate);
-    $(".ai-pd-container__trigger").on("pointerdown", function (event) {
-      if ($(this).hasClass("ai-pd-container__trigger--search")) {
-        $("#inffits_cblock").parent().fadeIn();
-      } else {
-        $("#inffits_cblock").parent().fadeOut();
-      }
-      $(this).toggleClass(
-        "ai-pd-container__trigger--search ai-pd-container__trigger--close"
-      );
-    });
+        document.body.insertAdjacentHTML("beforeend", aiSearchPdTemplate);
 
-    var breakpoint = 992;
+        // 獲取 iframe 的 contentWindow
+        const iframeElement = document.getElementById("inffits_tryon_window");
+        if (iframeElement) {
+          const iframe_container = iframeElement.contentWindow;
 
-    // 監聽窗口大小變化
-    window.addEventListener("resize", handleWindowResize);
+          // 確保 iframe 加載完成再傳送 postMessage
+          iframeElement.onload = () => {
+            const iframe_preview_obj = {
+              id: "INFS_All",
+              header: "from_preview",
+              brand: "INFS",
+            };
 
-    // 首次載入時執行檢查
-    handleWindowResize();
-    function handleWindowResize() {
-      // 取得當前視窗寬度
-      var windowWidth = window.innerWidth;
+            // 傳送 postMessage 到 iframe
+            iframe_container.postMessage(iframe_preview_obj, "*");
+          };
+        } else {
+          console.error("iframe 元素未找到，無法傳送 postMessage");
+        }
 
-      // 當視窗寬度大於等於 992px (桌面版)
-      if (windowWidth >= breakpoint) {
-      } else {
-      }
-    }
-  });
-})(jQuery);
+        $(".ai-pd-container__trigger").on("pointerdown", function (event) {
+          if ($(this).hasClass("ai-pd-container__trigger--search")) {
+            $("#inffits_cblock").parent().fadeIn();
+          } else {
+            $("#inffits_cblock").parent().fadeOut();
+          }
+          $(this).toggleClass(
+            "ai-pd-container__trigger--search ai-pd-container__trigger--close"
+          );
+        });
+
+        var breakpoint = 992;
+
+        // 監聽窗口大小變化
+        window.addEventListener("resize", handleWindowResize);
+
+        // 首次載入時執行檢查
+        handleWindowResize();
+        function handleWindowResize() {
+          // 取得當前視窗寬度
+          var windowWidth = window.innerWidth;
+
+          // 當視窗寬度大於等於 992px (桌面版)
+          if (windowWidth >= breakpoint) {
+          } else {
+          }
+        }
+      });
+    })(jQuery);
